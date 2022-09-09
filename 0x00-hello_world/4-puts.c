c5a0a782167c7c31be85b2568a9c2b264444c7a1
Hikma Endris <hikmuyemenusanu@gmail.com>
2022-09-09 14:50:10 +0300

    Add task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main()
+
+/**
+ * main - The main function
+ *
+ * Return: 0 for success
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle. \n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
