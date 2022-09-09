aef70bcec070d7f0707fe269f40bae7dfaabde95
Hikma Endris <hikmuyemenusanu@gmail.com>
2022-09-09 15:11:53 +0300

    Update task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - The main function
+ *
+ * Return: 0 for success
+ */
+int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
