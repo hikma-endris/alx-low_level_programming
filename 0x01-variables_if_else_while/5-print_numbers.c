57f175169bd45e451ead4432084d005974414c2c
Hikma Endris <hikmuyemenusanu@gmail.com>
2022-09-12 20:47:27 +0300

    Add task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
