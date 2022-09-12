1d87d52cd06ec21604eb8f6ed4925deffc55dc6a
Hikma Endris <hikmuyemenusanu@gmail.com>
2022-09-12 19:18:56 +0300

    Add task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	char i;
+
+	for(i='a'; i<='z'; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
