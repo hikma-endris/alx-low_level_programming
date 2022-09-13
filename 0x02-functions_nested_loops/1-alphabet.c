6c0a320c5ef38a5517db7f94d5651a27280d7731
Hikma Endris <hikmuyemenusanu@gmail.com>
2022-09-13 19:21:09 +0300

    Update task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void print_alphabet(void);
+
+/**
+ * main - The main function
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
