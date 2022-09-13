504963687cf4f47121624a5aec155a5374181c2a
Hikma Endris <hikmuyemenusanu@gmail.com>
2022-09-13 11:16:21 +0300

    Add task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define MAXSTRING 80
+
+/**
+ * main - The main function
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	char message[] = "_putchar";
+	int count;
+
+	for ( count = 0; count < MAXSTRING; count++)
+	{
+		if (message[count] == '\0')
+		{
+			putchar('\n');
+			break;
+		}
+		else
+			putchar(message[count]);
+	}
+	return (0);
+}
