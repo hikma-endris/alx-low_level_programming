fadd08fbf873cdbe3e1aa8716c43dda89978ba49
Hikma Endris <hikmuyemenusanu@gmail.com>
2022-09-17 15:40:12 +0300

    Udate task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	char i;
+	char j;
+
+        i = 'a';
+	j = '\n';
+	while (c <= 'z')
+	{
+		if ((i == 'e') || (i == 'q'))
+		{
+			i++;
+		}
+		putchar(i++);
+	}
+	putchar(j);
+	return (0);
+}
