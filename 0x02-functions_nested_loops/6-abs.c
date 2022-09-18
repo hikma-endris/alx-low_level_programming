723abd1f00df7530474f96105587c03642ca1c84
Hikma Endris <hikmuyemenusanu@gmail.com>
2022-09-18 19:36:27 +0300

    Add task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - print the absolute value of the integer
+ * @n: the integer used
+ * Return: the absolute value of the integer
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
