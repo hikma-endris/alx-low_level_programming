7fb0f88102be49f773fdec6da312937901723ca5
Hikma Endris <hikmuyemenusanu@gmail.com>
2022-09-20 21:17:41 +0300

    Update atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 #include <stdlib.h>
 
@@ -9,8 +8,23 @@
  */
 int _atoi(char *s)
 {
-	int n;
+	int i;
+	unsigned int num;
 
-	n = atoi(s);
-	return (n);
+	i = 1;
+	num = 0;
+
+	do {
+		if (*s == '-')
+			i *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * i);
 }
