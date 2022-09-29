7cb3ac9180b803281029f005c7286c764c15bd9b
Hikma Endris <hikmuyemenusanu@gmail.com>
2022-09-29 11:13:15 +0300

    Update task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,12 +2,13 @@
 
 /**
  * _puts_recursion - to print a string
+ * @*s: pointers of a string to be printed
  * Return - a given string
  */
 void _puts_recursion(char *s)
 {
 
-	if ( *s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
