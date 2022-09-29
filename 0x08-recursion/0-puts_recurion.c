565396e84bb5fcbb96160eab1a2f83aee1a8762e
Hikma Endris <hikmuyemenusanu@gmail.com>
2022-09-29 09:35:31 +0300

    Add task 0

---
+++ 0x08-recursion/0-puts_recurion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - to print a string
+ * Return - a given string
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	if ( i == *s)
+		return ("%d\n", *s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+task 0- function that prints a string
